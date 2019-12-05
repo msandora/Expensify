99bc6b83ad2f10773d1716a62e20bdc53bc9fc15
Mark Sandora <mark.sandora@gmail.com>
2019-12-05 16:58:02 -0500

    fix Datepicker bug

--- src/app.js
+++ src/app.js
@@ -15,9 +15,9 @@ import './styles/styles.scss';
 const store = configureStore();
 
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: 1575478800000 }));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1577034000000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 1575219600000 }));
 
 
 const state = store.getState();
